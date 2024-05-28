293aa5c12036839388be867ad28d402256522efe
DmitryBohan <zorrorul@gmail.com>
2024-05-28 19:08:50 +0300

    добавил к отображению окно сервера . на него поместил 2 кнопки старт и стоп

--- src/main/java/ServerWindow.java
+++ src/main/java/ServerWindow.java
@@ -1,9 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ServerWindow extends JFrame {
 
-    private boolean isConnected = false;
+    private JTextArea textArea;
+    private List<ClientGUI> clients = new ArrayList<>();
+    private boolean isRunning = false;
 
 
 
@@ -11,6 +16,17 @@ public class ServerWindow extends JFrame {
         setTitle("Chat Server");
         setSize(400, 300);
 
+        JPanel panel = new JPanel(new GridLayout(5, 2));
+
+        JButton startButton = new JButton("Start");
+        JButton stopButton = new JButton("Stop");
+
+        panel.add(startButton);
+        panel.add(stopButton);
+
+        add(panel,BorderLayout.SOUTH);
+
+        setVisible(true);
     }
 
     //метод включения сервера
