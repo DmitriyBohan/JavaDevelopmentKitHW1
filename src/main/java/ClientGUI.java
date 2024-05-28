61050809d649d8e2510c48c5c71eecee24ca89b9
DmitryBohan <zorrorul@gmail.com>
2024-05-28 14:59:53 +0300

    создал классы и создал необходимые методы без реализации

---
+++ src/main/java/ClientGUI.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+
+
+public class ClientGUI extends JFrame {
+
+
+
+    // Метод для подключения к серверу
+    void connectToServer() {
+
+    }
+
+    // Метод  отключения от сервера
+    void notifyDisconnection() {
+
+    }
+
+
+    // Метод для отправки сообщения на сервер
+    void sendMessage() {
+
+    }
+
+
+    //метод выводит сообщения в чат
+    void receiveMessage() {
+
+    }
+
+
+    //метод загрузки истории чата из файла
+    void loadChatHistory() {
+
+    }
+}
---
+++ src/main/java/ServerWindow.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+
+public class ServerWindow extends JFrame {
+
+
+
+    //метод включения сервера
+    void startServer() {
+
+    }
+
+    //метод выключающий сервер
+    void stopServer() {
+
+    }
+
+    // Метод для проверки запущен ли сервер
+    boolean isRunning() {
+        return false;
+    }
+
+    //добавляет клиента который успешно подключился
+    void addClient() {
+
+    }
+
+    //удаляем отключивщигося клиента
+    void removeClient() {
+
+    }
+
+    //метод отправки сообщений в общий чат
+    void broadcastMessage() {
+
+    }
+
+    //логирования истории сообщений
+    void logMessage() {
+
+    }
+
+    //метод загружающий чат из файла
+    void loadChatHistory(){
+
+    }
+
+}
+
